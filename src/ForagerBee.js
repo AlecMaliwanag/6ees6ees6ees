8543c0df91c0e6aea4f56e0a6ff817826e52c096
Alec Maliwanag <amaliwanag@gmail.com>
2016-09-19 12:03:44 -0700

    Implement es6 bees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,7 +1,17 @@
 import Bee from './Bee'
 
-class ForagerBee {
+class ForagerBee extends Bee{
   // TODO..
+  constructor () {
+    super();
+    this.age = 10;
+    this.job = "find pollen"
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage(item) {
+    this.treasureChest.push(item);
+  }
 };
 
 export default ForagerBee;
